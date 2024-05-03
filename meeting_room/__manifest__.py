e5cb245ca9523236bac80e670031af1caf6920a9
Nhat-113 <nhatvo1302@gmail.com>
2024-05-03 10:14:14 +0700

    mail

--- meeting_room/__manifest__.py
+++ meeting_room/__manifest__.py
@@ -6,14 +6,21 @@
     "description": """
         Meeting Room Management
     """,
-    "author": "Dung Vo Truong",
+    "author": "Intern FullStack",
     "website": "http://www.yourcompany.com",
     "category": "Uncategorized",
     "version": "0.1",
-    "depends": ["mail"],
+    "depends": ["mail","calendar"],
     "assets": {
         "web.assets_backend": [
             "meeting_room/static/src/css/meeting_room.css",
+            "meeting_room/static/js/schedule.js",
+            # "meeting_room/static/js/calendar_controller.js",
+        ],
+        'web.assets_qweb':[
+            # "meeting_room/static/xml/test.xml",
+            'meeting_room/static/xml/delete_event.xml',
+            # 'meeting_room/static/js/calendar_rendder.js'
         ],
     },
     "data": [
@@ -22,6 +29,8 @@
         "views/room_view.xml",
         "views/schedule_view.xml",
         "views/menu.xml",
+        'data/mail_template.xml',
+        
     ],
     "sequence": -100,
     "installable": True,
